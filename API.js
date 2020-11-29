cbfa7e4a680bebf4f2ce192177a60eac6c8802c3
fatma2607 <70535099+fatma2607@users.noreply.github.com>
2020-11-29 16:49:37 +0100

    hey

--- API.js
+++ API.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const fetch = require("node-fetch");
 const fs = require("fs");
 
-//Link til htmlll
+//Link til htmlllll
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
